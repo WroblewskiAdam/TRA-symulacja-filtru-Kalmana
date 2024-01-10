e7ff66b30c39f4f810a4c2b382c80a61286bfe72
Bargal1 <96236465+Bargal1@users.noreply.github.com>
2024-01-10 21:01:41 +0100

    kalman

--- proj - 2/Kalman_filter.m
+++ proj - 2/Kalman_filter.m
@@ -1,5 +1,5 @@
-function [X, P] = Kalman_filter (F, H, Q, R, y, X, P)
-    X_pred = F*X;
+function [X, P] = Kalman_filter (F, H, Q, G, u, R, y, X, P)
+    X_pred = F*X+G*u;
     P_pred = F*P*F' + Q;
     K = P_pred*H'*(H*P_pred*H'+R)^-1;
     X = X_pred + K*(y - H*X_pred);
